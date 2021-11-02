f297be4f7b4a70033d2f6dac3f756729440b8be8
Peter Polák <peter.polak.mail@gmail.com>
2021-11-02 07:40:58 +0100

    Refactor code and add offset

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float distanceDifferenceY = 1.0f;
+    [SerializeField] private float offsetY = 5.0f;
+    [SerializeField] private float speed = 2.0f;
     [SerializeField] private SpawnManager spawnManager;
     
     private Vector3 startingPosition;
@@ -12,16 +14,21 @@ public class CameraController : MonoBehaviour
     private void Start()
     {
         startingPosition = transform.position;
-        Debug.Log(startingPosition);
     }
 
     private void Update()
     {
         if (spawnManager.placedTetrominoes.Count == 0) return;
 
+        Tetromino highestTetromino = GetHighestTetromino();
+        MoveCamera(highestTetromino);
+    }
+
+    private Tetromino GetHighestTetromino()
+    {
         Tetromino highestTetromino = spawnManager.placedTetrominoes[0];
 
-        foreach(Tetromino tetromino in spawnManager.placedTetrominoes)
+        foreach (Tetromino tetromino in spawnManager.placedTetrominoes)
         {
             if (tetromino.gameObject.transform.position.y > highestTetromino.gameObject.transform.position.y)
             {
@@ -29,11 +36,16 @@ public class CameraController : MonoBehaviour
             }
         }
 
-        float difference = Mathf.Abs(highestTetromino.transform.position.y - transform.position.y);
+        return highestTetromino;
+    }
 
-        if(difference > 2)
+    private void MoveCamera(Tetromino highestTetromino)
+    {
+        float difference = Mathf.Abs(highestTetromino.transform.position.y + offsetY - transform.position.y);
+
+        if (difference > distanceDifferenceY)
         {
-            Vector3 direction = highestTetromino.transform.position.y > transform.position.y ? Vector3.up : Vector3.down;
+            Vector3 direction = highestTetromino.transform.position.y + offsetY > transform.position.y ? Vector3.up : Vector3.down;
             transform.Translate(direction * Time.deltaTime * speed);
         }
     }
